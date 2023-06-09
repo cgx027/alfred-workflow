20bd011bea1fe9374d47133530fcd0de7163c97e
Ted Chen <chente@vmware.com>
2023-06-09 20:21:31 +0800

    update timezone tool

--- timezone/timezone.py
+++ timezone/timezone.py
@@ -1,19 +1,40 @@
 import json
 import sys
 import re
+import pytz
+from typing import List
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from utils.date_time import *
 from utils.alfred import *
 
+@dataclass
+class CityTime:
+    city: str
+    time: datetime
+
+@dataclass
+class TimeItem:
+    current_city: CityTime
+    target_cities: List[CityTime]
+    is_current: bool
+
 CITIES = {
     "shanghai": {
         "timezone": 8,
-        "abbr": "SH"
+        "abbr": "SH",
+        "pst_enabled": False
     },
     "palo alto": {
         # "timezone": -8, # winter time
         "timezone": -7, # summer time
-        "abbr": "PA"
+        "abbr": "PA",
+        "pst_enabled": True
+    },
+    "bangalore": {
+        "timezone": 5.5,
+        "abbr": "BLR",
+        "pst_enabled": False
     }
 }
 # PA_SH_HOUR_DELTA = 16
@@ -21,59 +42,82 @@ TIME_FORMAT = "%b/%d %H:%M"
 HOURS_AHEAD = -4
 HOURS_FORWARD = 20
 CURRENT_CITY = 'shanghai'
-DEFAULT_TARGET_CITY = 'palo alto'
+DEFAULT_TARGET_CITIES = ['palo alto', 'bangalore']
 
-def get_target_city():
-    target_city = DEFAULT_TARGET_CITY
+def get_target_cities():
+    target_cities = []
 
     if len(sys.argv) >= 2:
         city_start_str = sys.argv[1]
+        if city_start_str:
+            for c in CITIES.keys():
+                if c.startswith(city_start_str.lower()):
+                    target_cities.append(c)
 
-        found_city = False
-        city_found = DEFAULT_TARGET_CITY
-        for c in CITIES.keys():
-            if c.startswith(city_start_str.lower()):
-                city_found = c
-                found_city = True
-        
-        if city_found:
-            target_city = city_found
+    if not target_cities:
+        # target_cities = DEFAULT_TARGET_CITIES
+        target_cities = copy.copy(DEFAULT_TARGET_CITIES)
+
+    # print('target_cities', target_cities)
+    return target_cities
+
+def is_pst_time():
+    us_tz = pytz.timezone('US/Pacific')
     
-    return target_city
+    # print("tzname: ", us_tz.localize(datetime.now()).tzname())
+    return us_tz.localize(datetime.now()).tzname() == 'PST'
 
-def get_timezone_list(current_time, target_city):
+def get_timezone_list(current_time, target_cities: list[str]) -> List[TimeItem]:
     # current_time = get_current_time(TIME_FORMAT)
-    tz_list = [] # list of tuples [SH_TIME, PA_TIME, is_current]
-    target_city_data = CITIES.get(target_city, 'palo alto')
-    target_city_tz = target_city_data.get('timezone', 8)
+    tz_list: List[TimeItem] = [] # list of tuples [SH_TIME, CITIES..., is_current]
+
     current_city_data = CITIES['shanghai']
     current_city_tz = current_city_data.get('timezone', 8)
-    timezone_delta = current_city_tz - target_city_tz
     
+    is_now_pst_time = is_pst_time()
+
+    # print("is_pst_summer_time", is_pst_time())
+
     for hour_delta in range(HOURS_AHEAD, HOURS_FORWARD):
-        # print('hour_delta', hour_delta)
-        # city_hour_delta = 
-        delta = timedelta(hours=hour_delta)
-        current_city_time = current_time + delta
-        target_city_time = current_city_time - timedelta(hours=timezone_delta)
+        target_cities_time: List[CityTime] = []
+
+        for target_city in target_cities:
+            target_city_data = CITIES.get(target_city, {})
+            if not target_city_data:
+                continue
+
+            target_city_tz = target_city_data.get('timezone', 8)
+            pst_enabled = target_city_data.get('pst_enabled', False)
+            if pst_enabled and is_now_pst_time:
+                target_city_tz -= 1
+
+            timezone_delta = current_city_tz - target_city_tz
+
+            # print('hour_delta', hour_delta)
+            delta = timedelta(hours=hour_delta)
+            current_city_time = current_time + delta
+            current_city_time_obj = CityTime('shanghai', current_city_time)
+
+            target_city_time = current_city_time - timedelta(hours=timezone_delta)
+
+            target_cities_time.append(CityTime(target_city, target_city_time))
+
         is_current = True if hour_delta == 0 else False
-
-        item = (current_city_time, target_city_time, target_city, is_current)
-        # print('item: ', item)
-        tz_list.append(item)
-
+        tz_list.append(TimeItem(current_city_time_obj, target_cities_time, is_current))
 
     return tz_list
 
-def format_alfred_item(item):
-    current_city_time, target_city_time, city, is_current = item
-    cur_time_str = convert_time_by_format(TIME_FORMAT, current_city_time)
-    tar_time_str = convert_time_by_format(TIME_FORMAT, target_city_time)
-    is_current_str = '*' if is_current else ''
-    cur_city_abbr = CITIES[CURRENT_CITY]['abbr']
-    tar_city_abbr = CITIES[city]['abbr']
+def format_alfred_item(item: TimeItem):
+    cur_time_str = convert_time_by_format(TIME_FORMAT, item.current_city.time)
+    cur_city_abbr = CITIES[item.current_city.city]['abbr']
+    is_current_str = '*' if item.is_current else ''
 
-    title = '{0} {1} - {2} {3} {4}'.format(cur_city_abbr, cur_time_str, tar_city_abbr, tar_time_str, is_current_str)
+    target_city_str = ""
+    for city in item.target_cities:
+        abbr = CITIES[city.city]['abbr']
+        target_city_str += f"{abbr} {convert_time_by_format(TIME_FORMAT, city.time)} "
+
+    title = '{0} {1} - {2} {3}'.format(cur_city_abbr, cur_time_str, target_city_str, is_current_str)
     # subtitle = 'Current' if is_current else ''
     subtitle = ''
 
@@ -81,9 +125,9 @@ def format_alfred_item(item):
     return item
 
 # print('city:', target_city)
-target_city =  get_target_city()
+target_city =  get_target_cities()
 time_list = get_timezone_list(datetime.now(), target_city)
 alfred_list = compose_alfred_list(time_list, format_alfred_item)
 
-# print(json.dumps(alfred_list, indent=4))
-print(json.dumps(alfred_list))
+print(json.dumps(alfred_list, indent=4))
+# print(json.dumps(alfred_list))
